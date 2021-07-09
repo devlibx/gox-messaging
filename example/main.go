b039b26eee4e4e10e356918ee19a75c9c795fd99
Harish Bohara <you@example.com>
2021-07-09 14:41:11 +0530

    added kafka test

--- example/main.go
+++ example/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/devlibx/gox-base"
+	messaging "github.com/devlibx/gox-messaging"
+	"github.com/devlibx/gox-messaging/kafka"
+	"github.com/google/uuid"
 	"go.uber.org/zap"
+	"time"
 )
 
 func main() {
@@ -11,8 +17,45 @@ func main() {
 	cf := gox.NewCrossFunction(zapConfig.Build())
 
 	// Send SQS message
-	err := SqsSendMessage(cf)
+	// err := SqsSendMessage(cf)
+	err := KafkaSendMessage(cf)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func KafkaSendMessage(cf gox.CrossFunction) error {
+
+	// Setup 1 - create a producer
+	producerConfig := messaging.ProducerConfig{
+		Name:        "test_queue",
+		Type:        "kafka",
+		Endpoint:    "localhost:9092",
+		Topic:       "harish_test",
+		Concurrency: 1,
+		Enabled:     true,
+		Async:       false,
+		Properties:  gox.StringObjectMap{messaging.KMessagingPropertyPublishMessageTimeoutMs: 100000, messaging.KMessagingPropertyAcks: "1"},
+	}
+
+	producer, err := kafka.NewKafkaProducer(cf, producerConfig)
+	if err != nil {
+		return err
+	}
+
+	contextWithTimeout, contextCancelFunction := context.WithTimeout(context.Background(), 1*time.Second)
+	defer contextCancelFunction()
+
+	// Send a message
+	id := uuid.NewString()
+	response := <-producer.Send(contextWithTimeout, &messaging.Message{
+		Key:     "key",
+		Payload: map[string]interface{}{"key": "value", "id": id},
+	})
+	if response.Err != nil {
+		return response.Err
+	}
+	fmt.Println(response.RawPayload)
+	time.Sleep(time.Second)
+	return nil
 }
