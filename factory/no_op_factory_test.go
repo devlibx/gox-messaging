8ce3e322baabd92dbcaf2cec516c075fb85217d9
Harish Bohara <harish.bohara@gmail.com>
2023-06-10 00:51:39 +0530

    [*] added a no op message factory

---
+++ factory/no_op_factory_test.go
@@ -0,0 +1,20 @@
+package factory
+
+import (
+	"context"
+	messaging "github.com/devlibx/gox-messaging"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNoOpFactory(t *testing.T) {
+	mf := NewNoOpMessagingFactory()
+	err := mf.Start(messaging.Configuration{})
+	assert.NoError(t, err)
+
+	p, err := mf.GetProducer("dummy")
+	assert.NoError(t, err)
+	res := p.Send(context.Background(), &messaging.Message{Key: "user_1", Payload: "1234"})
+	assert.NotNil(t, res)
+	assert.NotNil(t, <-res)
+}
