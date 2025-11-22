afd16607398d0c2ffee2d78b2ce5693a775c8c63
Harish Bohara <harish.bohara@gmail.com>
2025-11-22 18:42:43 +0530

    Added perf for pubsub

--- example/types/pubsub_send_message.go
+++ example/types/pubsub_send_message.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/devlibx/gox-base/v2"
@@ -41,16 +43,40 @@ func PubSubSendMessage(cf gox.CrossFunction) error {
 		return err
 	}
 
-	// Send a message
-	id := uuid.NewString()
-	response := <-producer.Send(context.Background(), &messaging.Message{
-		Key:     "key-harish-",
-		Payload: map[string]interface{}{"key": "value", "id": id, "time": time.Now().String()},
-	})
-	if response.Err != nil {
-		return response.Err
+	threads := 10
+	const maxMessages int64 = 10000
+
+	start := time.Now()
+	var messages int64 = 0
+	wg := &sync.WaitGroup{}
+	for i := 0; i < threads; i++ {
+		wg.Add(1)
+		go func() {
+			for {
+				count := atomic.AddInt64(&messages, 1)
+				if count >= maxMessages {
+					wg.Done()
+					break
+				}
+				// Send a message
+				id := uuid.NewString()
+				response := <-producer.Send(context.Background(), &messaging.Message{
+					Key:     "key-harish-",
+					Payload: map[string]interface{}{"key": "value", "id": id, "time": time.Now().String()},
+				})
+				if response.Err != nil {
+					fmt.Printf("%d - Sent message with ID = %s\n", count, err.Error())
+				} else {
+					if count%1000 == 0 {
+						fmt.Printf("%d - Sent message with ID = %s\n", count, response.RawPayload)
+					}
+				}
+			}
+		}()
 	}
-	fmt.Println("Sent message with ID:", response.RawPayload)
+	wg.Wait()
+	end := time.Now()
+	fmt.Printf("Published %d messages in %v\n", messages, end.Sub(start))
 
 	// Setup 2 - create a consumer
 	consumerConfig := messaging.ConsumerConfig{
@@ -70,22 +96,41 @@ func PubSubSendMessage(cf gox.CrossFunction) error {
 	}
 
 	// Start consumer
+	start = time.Now()
+	var consumedMessages int64 = 0
 	err = consumer.Process(context.Background(), messaging.NewSimpleConsumeFunction(cf, "my-consumer",
 		func(message *messaging.Message) error {
-			fmt.Println("Received message:", message.Payload)
-			fmt.Println(gox.StringObjectMapFromJson(string(message.Payload.([]byte))))
+			count := atomic.AddInt64(&consumedMessages, 1)
+			if count%1000 == 0 {
+				fmt.Println("Received message:", message.Payload)
+				fmt.Println(gox.StringObjectMapFromJson(string(message.Payload.([]byte))))
+			}
 			return nil
 		},
 		func(message *messaging.Message, err error) {
-			fmt.Println("Error processing message:", err)
+			count := atomic.AddInt64(&consumedMessages, 1)
+			if count%1000 == 0 {
+				fmt.Println("Error processing message:", err)
+				fmt.Println(gox.StringObjectMapFromJson(string(message.Payload.([]byte))))
+			}
 		},
 	))
 	if err != nil {
 		return err
 	}
+	for {
+		count := atomic.AddInt64(&consumedMessages, 0)
+		if count >= maxMessages-10 {
+			break
+		} else {
+			time.Sleep(1 * time.Second)
+		}
+	}
+	end = time.Now()
+	fmt.Printf("Consumed %d messages in %v\n", consumedMessages, end.Sub(start))
 
 	// Wait for a message to be received
-	time.Sleep(10 * time.Second)
+	time.Sleep(60 * time.Second)
 
 	return nil
 }
